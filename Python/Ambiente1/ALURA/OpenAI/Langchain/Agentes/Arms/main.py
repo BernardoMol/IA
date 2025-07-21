eca528df51f42d26283ee692063ae106434daa60
BernardoMol <bernardobcmb@gmail.com>
2025-07-21 08:17:45 -0300

    Agente arms

--- Python/Ambiente1/ALURA/OpenAI/Langchain/Agentes/Arms/main.py
+++ Python/Ambiente1/ALURA/OpenAI/Langchain/Agentes/Arms/main.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# pergunta = "Quero viajar com minha noiva dia 13/06/2026. Quero visitar os melhores restaurantes e locais romanticos. Qual a melhor cidade?"
-pergunta = "Quero viajar com meu pai no final deste ano. Gostamos de cerveja, interior, fazendas, roça essas coisasa. Qual a melhor cidade?"
+pergunta = "Quero viajar com minha noiva dia 13/06/2026. Quero visitar os melhores restaurantes e locais romanticos. Qual a melhor cidade?"
 
 agente = ARMSagenteOpenAI()
 executor = AgentExecutor(agent=agente.agente,
