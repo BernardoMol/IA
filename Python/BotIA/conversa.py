f5de156e4a4ed6a2f0affdb374c0f5911b4a289c
BernardoMol <bernardobcmb@gmail.com>
2025-08-29 10:09:21 -0300

    Gerando conversa com loop while

---
+++ Python/BotIA/conversa.py
@@ -0,0 +1,50 @@
+import os  # biblioteca do sistema operacilnal # permite acessar nossas chaves de acesso
+import google.generativeai as genai  # biblioteca do google
+# importa as variaveis de ambiente que estão no ".env"
+from dotenv import load_dotenv
+load_dotenv()  # carrega as chaves de acesso
+
+# Pré configurações
+CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
+MODELO_ESCOLHIDO = "gemini-2.5-pro"
+
+
+def categorizar_produtos(mensagem, categorias):
+    prompt_sistema = f"""
+                Você é um categorizador de alimentos.
+                Você deve assumir as categorias presentes na lista abaixo.
+                # Lista de Categorias Válidas
+                {categorias.split(",")}
+                # Formato da Saída
+                Produto: Nome do Alimento
+                Categoria: apresente a categoria do alimento
+                # Exemplo de Saída
+                Alimento: Banana
+                Categoria: Saldáveis, naturais
+            """
+
+    # Montagem da LLM
+    genai.configure(api_key=CHAVE_API_GOOGLE)
+    llm = genai.GenerativeModel(
+        model_name=MODELO_ESCOLHIDO,
+        system_instruction=prompt_sistema,
+    )
+    resposta = llm.generate_content(mensagem)
+    return resposta.text
+
+
+def main():
+    lista_de_categorias = "Saldáveis, processados, doces, salgados, naturais"
+    # Comando e resposta
+    pergunta = input("Informe os alimentos para classificar: ")
+    while pergunta != "":
+        print(
+            f"Resposta: \n{categorizar_produtos(pergunta, lista_de_categorias)}"
+        )
+        pergunta = input("Informe o produto que você deseja classificar: ")
+
+    print(f"RESPOSTA FINAL: OBRIGADO PELA CONVERSA")
+
+
+if __name__ == "__main__":
+    main()
